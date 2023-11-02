53e322b71cb6f24add4da7fa157a638f2ddea5a5
Nouxx <clement.vannouque@gmail.com>
2023-11-01 22:27:48 -0400

    fix env for tests

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -3,7 +3,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	reporter: 'html',
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'npm run build -- --mode playwright && npm run preview',
 		port: 4173
 	},
 	testDir: 'tests',
