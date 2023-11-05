7aa3162c8683984b81e812cfc6f058e6bf775893
Nouxx <clement.vannouque@gmail.com>
2023-11-05 16:27:36 -0500

    feat: enable google sign in
    Closes #3

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -4,3 +4,21 @@
 
 <h1>Blazing fire</h1>
 <p>Environment: {PUBLIC_ENVIRONMENT}</p>
+
+<div id="g_id_onload"
+     data-client_id=" 291462974388-uu40umo2shubgklqt49ogofod0n9527o.apps.googleusercontent.com"
+     data-context="signin"
+     data-ux_mode="popup"
+     data-login_uri="http://localhost:5173"
+     data-auto_prompt="false">
+</div>
+
+<div class="g_id_signin"
+     data-type="standard"
+     data-shape="pill"
+     data-theme="outline"
+     data-text="signin_with"
+     data-size="large"
+     data-logo_alignment="left"
+	 data-width="100">
+</div>
