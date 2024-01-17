5a507a8521aad5678f7bb5561dce19d743832027
Nouxx <clement.vannouque@gmail.com>
2024-01-17 18:40:21 -0500

    enable server hooks log

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -1,7 +1,7 @@
 // https://firebase.google.com/docs/auth/web/redirect-best-practices#handle-signin-independently
 
 export async function handle({ event, resolve }) {
-	// console.log(`hook : ${event.url.pathname}`);
+	console.log(`hook : ${event.url.pathname}`);
 	const response = await resolve(event);
 	return response;
 }
