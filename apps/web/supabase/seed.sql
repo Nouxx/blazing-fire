d2c348d178db9aeba13cc92670a08c35fcba5f18
Nouxx <clement.vannouque@gmail.com>
2024-03-24 14:24:22 -0400

    remove func from dump

--- apps/web/supabase/seed.sql
+++ apps/web/supabase/seed.sql
@@ -167,22 +167,7 @@ SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 136, true);
 
 
 --
--- Custom: get_user_id_by_email function
---
-
-CREATE OR REPLACE FUNCTION get_user_id_by_email(email TEXT)
-RETURNS TABLE (id uuid)
-SECURITY definer
-AS $$
-BEGIN
-  RETURN QUERY SELECT au.id FROM auth.users au WHERE au.email = $1;
-END;
-$$ LANGUAGE plpgsql;
-
---
 -- PostgreSQL database dump complete
 --
 
 RESET ALL;
-
-
