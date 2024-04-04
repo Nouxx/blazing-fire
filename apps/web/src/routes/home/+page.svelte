c1bfdf8c57ec07a1d5a0380d14e74551927bcfd4
Nouxx <clement.vannouque@gmail.com>
2024-04-03 22:29:17 -0400

    test: update screenshots

--- apps/web/src/routes/home/+page.svelte
+++ apps/web/src/routes/home/+page.svelte
@@ -1,3 +1,6 @@
 <div class="flex flex-col items-center mx-5">
 	<h1 class="text-3xl font-bold my-3">Home</h1>
+	<div class="flex flex-col items-center mt-2">
+		<p class="text-sm my-1">There is nothing here to do, yet.</p>
+	</div>
 </div>
