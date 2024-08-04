edd1b400870c387edfb297c8f0c8d7b6989070ee
Nouxx <clement.vannouque@gmail.com>
2024-08-03 22:15:06 -0400

    fix types script

--- apps/web/scripts/db-types.sh
+++ apps/web/scripts/db-types.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 PGHOST=localhost
+PGPORT=$POSTGRES_PORT
 PGUSER=postgres
-PGPASSWORD=your-super-secret-and-long-postgres-password
-PGDATABASE=postgres
+PGPASSWORD=$POSTGRES_PASSWORD
+PGDATABASE=$POSTGRES_DB
 
 echo "Generating types for database: '$PGDATABASE', host: '$PGHOST' with user: '$PGUSER'"
 
 npx supabase gen types \
-	--db-url postgresql://$PGUSER:$PGPASSWORD@$PGHOST/$PGDATABASE 
+	--db-url postgresql://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE \
 	--lang=typescript > src/lib/database/generated-types.ts
