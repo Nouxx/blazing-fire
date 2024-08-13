220062b53972b4eae10e26e91b48e60c892424e3
Nouxx <clement.vannouque@gmail.com>
2024-08-13 07:29:55 -0400

    wip

--- apps/web/src/lib/components/Header.svelte
+++ apps/web/src/lib/components/Header.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import { routes } from '$lib/const/routes';
+	import Logo from '$lib/icons/Logo.svelte';
 	import type { Session } from '@supabase/supabase-js';
 	export let session: Session | null;
 </script>
 
-<div class="flex flex-col sticky top-0" data-testid="header">
-	<div class="flex flex-row items-center bg-slate-50 py-3 border-b-4 border-slate-400">
+<div class="header flex flex-col sticky top-0" data-testid="header">
+	<div class="flex flex-row items-center py-3 border-b-4 border-slate-400">
 		<div class="px-2 flex-1">
-			<div>
-				<p class="font-bold">LOGO</p>
+			<div class="flex flex-row header__logo">
+				<Logo />
+				<!-- <p class="flex items-center logo__text">Blazing Fire</p> -->
 			</div>
 		</div>
 
@@ -22,3 +24,22 @@
 	</div>
 </div>
 <div />
+
+<style lang="scss">
+	.header {
+		background: linear-gradient(to bottom right, #0d3b66, #10497e, #135490);
+
+		&__logo:hover {
+			opacity: 0.5;
+			cursor: pointer;
+		}
+	}
+
+	.logo__text {
+		padding-left: 0.5rem;
+		text-transform: uppercase;
+		font-size: 1.5rem;
+		font-weight: bold;
+		color: #fca152;
+	}
+</style>
