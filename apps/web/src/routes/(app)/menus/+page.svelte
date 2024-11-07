cac28be9f52467906a8a33ee8cea8ecae011cbed
Nouxx <clement.vannouque@gmail.com>
2024-11-07 08:09:18 +0100

    wip

--- apps/web/src/routes/(app)/menus/+page.svelte
+++ apps/web/src/routes/(app)/menus/+page.svelte
@@ -23,9 +23,9 @@
 	}
 </script>
 
-<div class="flex flex-col items-center mx-5" data-testid="menu-page">
-	<div class="flex flex-row w-full my-6 items-center">
-		<h1 class="flex flex-1 text-3xl font-bold">Menus</h1>
+<div class="page" data-testid="menu-page">
+	<div class="page__header">
+		<h1 class="page__header-title">Menus</h1>
 		<Button
 			variant="primary"
 			label={isEditionOn ? 'Back' : 'Edit'}
@@ -34,7 +34,11 @@
 		/>
 	</div>
 
-	{#if thereIsNoMenu}
+	<div class="page__main">
+		<p>YOLO</p>
+	</div>
+
+	<!-- {#if thereIsNoMenu}
 		<p class="my-4">You don't have any menu yet.</p>
 	{:else}
 		{#each data.menus as menu (menu.id)}
@@ -44,5 +48,49 @@
 
 	{#if !isEditionOn}
 		<MenuCreateAction />
-	{/if}
+	{/if} -->
 </div>
+
+<style lang="scss">
+	.page {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding-left: 1.25rem;
+		padding-right: 1.25rem;
+		width: 100%;
+		height: 100vh;
+
+		&__header {
+			display: flex;
+			flex-direction: row;
+			padding-top: 1.5rem;
+			padding-bottom: 1.5rem;
+			align-items: center;
+			width: 100%;
+
+			&-title {
+				display: flex;
+				flex: 1;
+				font-size: 1.875rem;
+				line-height: 2.25rem;
+				font-weight: 700;
+			}
+		}
+
+		&__main {
+			width: 100%;
+			margin-top: 1.5rem;
+			border-top-left-radius: 2rem;
+			border-top-right-radius: 2rem;
+			background-color: var(--color-background-secondary);
+			display: flex;
+			flex-direction: column;
+			flex: 1;
+		}
+	}
+
+	.debug {
+		border: 1px solid violet;
+	}
+</style>
