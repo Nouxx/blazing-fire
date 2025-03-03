13936950ffedc2c68622db2788af380e6a3d04d1
Nouxx <clement.vannouque@gmail.com>
2025-03-03 22:59:51 +0100

    remove footer

--- apps/web/src/lib/components/Footer.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { PUBLIC_VERSION } from '$env/static/public';
-
-	const version = PUBLIC_VERSION;
-</script>
-
-<footer>
-	<p>version: {version}</p>
-</footer>
--- apps/web/src/routes/(app)/+layout.svelte
+++ apps/web/src/routes/(app)/+layout.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 </script>
 
 <Header />
 
 <slot />
-
-<Footer />
